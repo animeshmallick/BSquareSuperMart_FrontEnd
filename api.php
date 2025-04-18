429f8fa3d584955e99604a58b743dee70735ddac
= <animesh7995@gmail.com>
2025-04-18 14:34:06 +0530

    minor fix

--- api.php
+++ api.php
@@ -1,11 +1,31 @@
 <?php
-
 class api
 {
+    private $protocol;
+    private $localhost;
+    private $port;
+    function __construct(){
+        $this->protocol = 'http';
+        $this->localhost = "localhost";
+        $this->port = 7777;
+    }
+    function get_end_point(): string {
+        return $this->protocol . '://' . $this->localhost . ':' . $this->port;
+    }
+    /**
+     * @throws Exception
+     */
     public function getAllCategories(){
-        $ch = curl_init("http://localhost:7777/categories");
+        $url =
+        $ch = curl_init($this->get_end_point() . "/categories");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($ch);
+        if (curl_errno($ch)) {
+            // Fall back to PROD API
+            curl_close($ch);
+            throw new Exception("!!! == BackEnd Server Not Running == !!!");
+
+        }
         curl_close($ch);
         return json_decode($response, true);
     }
