6a5979e3dd1632a49d90e0fc81cc3e923cbcffb6
Subhrajeet <prakashparijat@gmail.com>
2025-04-26 17:11:54 +0530

    add category products page

--- category/index.php
+++ category/index.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Quick Commerce - Biscuits</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <title>Product Category</title>
     <link href="style.css" rel="stylesheet"/>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="script.js"></script>
+    <script src="../scripts.js"></script>
 </head>
 <body>
+
 <div>
     <div class="delivery_header"><h4>Delivery in 7 minutes</h4></div>
      <div class="wrapper">
@@ -24,12 +23,10 @@
         </div>
      </div>
 </div>
-
 <div class="cart-bar">
-    <div class="cart-items" id="cartItems">0 items in cart</div>
+    <div><span id="cartItemsCount"></span><span> items in cart</div>
     <a href="#" class="btn btn-primary">Go to Cart</a>
 </div>
 
 </body>
-
 </html>
