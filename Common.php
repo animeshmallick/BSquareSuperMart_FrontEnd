d000dfd83411515f2f4d7eff9878df84274a1de9
= <animesh7995@gmail.com>
2025-05-20 21:25:45 +0530

    fix is_user_login()

--- Common.php
+++ Common.php
@@ -1,7 +1,23 @@
 <?php
 class Common {
-    public function is_user_logged_in($customer_id): bool
+    public function is_user_logged_in($authToken): bool
     {
-        return $customer_id !== null && strlen($customer_id) > 0;
+        if($authToken === null)
+            return false;
+        if(strlen($authToken) <= 0)
+            return false;
+        $auth = array("X-Authorization" => "Bearer " . $authToken);
+        $api = (new ApiBuilder())
+            ->init()
+            ->setMethod('POST')
+            ->setPath("/isValidToken")
+            ->setHeaders($auth)
+            ->execute();
+        $response = $api->getResponse();
+        if(isset($response->error))
+            return false;
+        if(!isset($response->is_valid_user))
+            return false;
+        return $response->is_valid_user === true;
     }
 }
