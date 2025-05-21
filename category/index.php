af7fbf9d3c44858938efeb3c56e29ed25e1d5d7a
Subhrajeet <prakashparijat@gmail.com>
2025-05-21 23:16:32 +0530

    category page

--- category/index.php
+++ category/index.php
@@ -1,3 +1,15 @@
+<?php
+include "../Common.php";
+include "../ApiBuilder.php";
+$category_param = $_GET['category'] ?? null;
+$path = "/category/".$category_param;
+$api = (new ApiBuilder())->init()
+    ->setMethod("GET")
+    ->setPath($path)
+    ->execute();
+$category_products = $api->getResponse();
+$subcategories = array_keys(get_object_vars($category_products));
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,27 +19,55 @@
     <link href="style.css" rel="stylesheet"/>
     <link href="../styles.css" rel="stylesheet"/>
     <script src="../Config.js"></script>
-    <script src="script.js"></script>
     <script src="../scripts.js"></script>
+    <script src="script.js"></script>
 </head>
 <body>
 
-<div>
-    <div class="delivery_header"><h4>Delivery in 7 minutes</h4></div>
+<div class="page-container">
+    <div class="category-header" id="categoryHeader" style="flex-direction: row">
+        <button class="back-button" onclick="handleBackButton()">←</button>
+        <h2><?= $category_param ?></h2>
+    </div>
      <div class="wrapper">
         <div class="sidebar" id="sidebarList">
-            <!-- Dynamic sidebar population  -->
+            <?php foreach($subcategories as $subcategory){ ?>
+                <div class="sidebar-subcategory-item" id="<?= $subcategory ?>" onclick="displayProductsForSubcategory('<?= $subcategory ?>')">
+                    <img src="https://via.placeholder.com/30" alt="<?= $subcategory ?>"> <h6><?= $subcategory ?></h6>
+                </div>
+            <?php }?>
         </div>
         <div class="content" id="content">
             <div class="row" id="productList">
-                <!-- Dynamic products will be injected here -->
+                <?php foreach($category_products as $subcategory_products => $products){ ?>
+                    <?php foreach($products as $product){ ?>
+                        <div class="product-outer-container" style="display: none" subcategory="<?= $subcategory_products ?>">
+                            <div class="image-quantity">
+                            <img src="<?= $product->productImg ?>" class="square-image" alt="<?= $product->productName ?>">
+                                <div>
+                                    <div><?= $product->productSize ?> </div>
+                                    <div><strong>MRP ₹<?= $product->productPrice ?></strong> <s>₹<?= $product->productMrp ?></s></div>
+                                </div>
+                                <div id="addProduct_<?= $product->productId ?>">
+                                    <script>addProductQuantityContainer(<?= $product->productId ?>);</script>
+                                </div>
+                            </div>
+                            <div>
+                                <h5 class="productDescription"><?= $product->productName ?></h5>
+                            </div>
+                        </div>
+                    <?php } ?>
+                <?php } ?>
             </div>
         </div>
+         <script>
+             displayProductsForSubcategory('<?= $subcategories[0] ?>')
+         </script>
      </div>
 </div>
 <div class="cart-bar">
     <div><span id="cartItemsCount"></span><span> items in cart</div>
-    <a href="#" class="btn btn-primary">Go to Cart</a>
+    <a href="../cart" class="btn btn-primary">Go to Cart</a>
 </div>
 
 </body>
