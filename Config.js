085d5eeab6cf84d087ead843b93247e9484cff86
Subhrajeet <prakashparijat@gmail.com>
2025-06-16 00:16:25 +0530

    Fix Config

--- Config.js
+++ Config.js
@@ -1,13 +1,16 @@
 const CONFIG = {
-    BACKEND_ENV: "PROD", //"PROD" for Production backend and "local" for local backend
+    BACKEND_ENV: "QA", //"PROD" for Production backend and "local" for local backend
 
-
-    BACKEND_PROTOCOL: "http",
-    BACKEND_LOCAL_IP: "localhost",
-    BACKEND_PROD_IP: "3.110.204.162",
-    BACKEND_PORT: 7777,
+    BACKEND_LOCAL_IP: "localhost:7777",
+    BACKEND_QA_IP: "qa.bsquaresupermart.in",
+    BACKEND_PROD_IP: "www.bsquaresupermart.in"
 }
-const BACKEND_URI = CONFIG.BACKEND_PROTOCOL + "://" +
-    (CONFIG.BACKEND_ENV === "PROD" ? CONFIG.BACKEND_PROD_IP : CONFIG.BACKEND_LOCAL_IP) + ":" +
-    CONFIG.BACKEND_PORT
-const BACKEND_IP = CONFIG.BACKEND_ENV === "PROD" ? CONFIG.BACKEND_PROD_IP : CONFIG.BACKEND_LOCAL_IP;
+
+let BACKEND_URI = null;
+if(CONFIG.BACKEND_ENV === "LOCAL"){
+    BACKEND_URI = "http://" + CONFIG.BACKEND_LOCAL_IP;
+}else if(CONFIG.BACKEND_ENV === "QA"){
+    BACKEND_URI = "https://" + CONFIG.BACKEND_QA_IP + "/api";
+}else if(CONFIG.BACKEND_ENV === "PROD"){
+    BACKEND_URI = "https://" + CONFIG.BACKEND_PROD_IP + "/api";
+}
